06738a097cfc2d1ad42af9119b6c462fb92d69a8
cfiladelfo <119968249+Cfiladelfo@users.noreply.github.com>
2022-12-18 20:09:34 +0000

    06- entrada de dados

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
-var salario_bruto = 7500;
-var ir = salario_bruto *0.10;
-var salario_liquido  = salario_bruto - ir;
-console.log("Salario Liquido R$",salario_liquido);
+const prompt = require('prompt-sync')()
+
+var nome = prompt("Digite seu Nome:  ")
+var salario_bruto = parseFloat(prompt("Digite seu Salario Bruto:   "));
+var ir = salario_bruto * 0.10;
+var salario_liquido = salario_bruto - ir;
+console.log("Salario Liquido R$", salario_liquido);
+
