46fd4a09371b8d27892411b09655d1160e0e1d41
cfiladelfo <119968249+Cfiladelfo@users.noreply.github.com>
2022-12-19 00:01:14 +0000

    VERSÃO 07 - COMANDO DE DECIsÃO

--- index.js
+++ index.js
@@ -2,7 +2,17 @@ const prompt = require('prompt-sync')()
 
 var nome = prompt("Digite seu Nome:  ")
 var salario_bruto = parseFloat(prompt("Digite seu Salario Bruto:   "));
-var ir = salario_bruto * 0.10;
+
+if(salario_bruto > 0 && salario_bruto <= 1903.98){
+  var ir = salario_bruto * 0.05;
+}else if( salario_bruto <= 2826.65){
+  var ir = salario_bruto * 0.75;
+}else{
+  var ir = salario_bruto * 0.15;
+}
+
+
+
 var salario_liquido = salario_bruto - ir;
 console.log("Salario Liquido R$", salario_liquido);
 
