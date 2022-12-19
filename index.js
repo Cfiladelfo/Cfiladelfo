ae3fc7a8042f01f1ba71c5d2fb90f5291b4a68a3
cfiladelfo <119968249+Cfiladelfo@users.noreply.github.com>
2022-12-19 01:24:13 +0000

    VERSÃO 08 - COMANDO DE DECIsÃO ANINHADAS

--- index.js
+++ index.js
@@ -2,15 +2,19 @@ const prompt = require('prompt-sync')()
 
 var nome = prompt("Digite seu Nome:  ")
 var salario_bruto = parseFloat(prompt("Digite seu Salario Bruto:   "));
+var dependentes = parseInt(prompt("Digite o numero de dependentes:  "))
+var renda_percapta = salario_bruto / (dependentes+1)
 
-if(salario_bruto > 0 && salario_bruto <= 1903.98){
-  var ir = salario_bruto * 0.05;
-}else if( salario_bruto <= 2826.65){
-  var ir = salario_bruto * 0.75;
-}else{
-  var ir = salario_bruto * 0.15;
-}
 
+if (renda_percapta >= 500) {
+  if (salario_bruto > 0 && salario_bruto <= 1903.98) {
+    var ir = salario_bruto * 0.05;
+  } else if (salario_bruto <= 2826.65) {
+    var ir = salario_bruto * 0.75;
+  } else {
+    var ir = salario_bruto * 0.15;
+  }
+} else { var ir = 0; }
 
 
 var salario_liquido = salario_bruto - ir;
