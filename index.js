b5c98fc303ee83ae59f4a93efddb8e409c812325
cfiladelfo <119968249+Cfiladelfo@users.noreply.github.com>
2022-12-20 18:21:29 +0000

    VERSÃO 09 - COMANDO DE repetição

--- index.js
+++ index.js
@@ -1,22 +1,28 @@
 const prompt = require('prompt-sync')()
 
-var nome = prompt("Digite seu Nome:  ")
-var salario_bruto = parseFloat(prompt("Digite seu Salario Bruto:   "));
-var dependentes = parseInt(prompt("Digite o numero de dependentes:  "))
-var renda_percapta = salario_bruto / (dependentes+1)
 
+for (i = 1; i <= 5 ; i++){
+  console.log("Pessoa: ", i)
+  var nome = prompt("Digite seu Nome:  ")
+  var salario_bruto = parseFloat(prompt("Digite seu Salario Bruto:   "));
+  var dependentes = parseInt(prompt("Digite o numero de dependentes:  "))
+  var renda_percapta = salario_bruto / (dependentes+1)
+  
+  
+  if (renda_percapta >= 500) {
+    if (salario_bruto > 0 && salario_bruto <= 1903.98) {
+      var ir = salario_bruto * 0.05;
+    } else if (salario_bruto <= 2826.65) {
+      var ir = salario_bruto * 0.75;
+    } else {
+      var ir = salario_bruto * 0.15;
+    }
+  } else { var ir = 0; }
+  
+  
+  var salario_liquido = salario_bruto - ir;
+  console.log("Salario Liquido R$", salario_liquido);
+  
 
-if (renda_percapta >= 500) {
-  if (salario_bruto > 0 && salario_bruto <= 1903.98) {
-    var ir = salario_bruto * 0.05;
-  } else if (salario_bruto <= 2826.65) {
-    var ir = salario_bruto * 0.75;
-  } else {
-    var ir = salario_bruto * 0.15;
-  }
-} else { var ir = 0; }
-
-
-var salario_liquido = salario_bruto - ir;
-console.log("Salario Liquido R$", salario_liquido);
-
+   
+}
