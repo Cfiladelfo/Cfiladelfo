3026d1441f37b5e4eb1c4fca121e2c03ed5c63ef
cfiladelfo <119968249+Cfiladelfo@users.noreply.github.com>
2022-12-26 20:14:51 +0000

    VERSÃO 10 - COMANDO DE REPETIÇÃO ANINHADO

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
 const prompt = require('prompt-sync')()
 
 
-for (i = 1; i <= 5 ; i++){
+for (i = 1; i <= 5; i++) {
   console.log("Pessoa: ", i)
   var nome = prompt("Digite seu Nome:  ")
   var salario_bruto = parseFloat(prompt("Digite seu Salario Bruto:   "));
   var dependentes = parseInt(prompt("Digite o numero de dependentes:  "))
-  var renda_percapta = salario_bruto / (dependentes+1)
+
+for (j = 1; j <= dependentes; j++){
+  var ganho = parseInt(prompt("Digite seu ganho mensal: R$"));salario_bruto = ganho + salario_bruto
+}
   
+  var renda_percapta = salario_bruto  / (dependentes + 1)
+
   
+
+
   if (renda_percapta >= 500) {
     if (salario_bruto > 0 && salario_bruto <= 1903.98) {
       var ir = salario_bruto * 0.05;
@@ -18,11 +25,13 @@ for (i = 1; i <= 5 ; i++){
       var ir = salario_bruto * 0.15;
     }
   } else { var ir = 0; }
-  
-  
+
+
   var salario_liquido = salario_bruto - ir;
   console.log("Salario Liquido R$", salario_liquido);
-  
 
-   
+
+
 }
+
+
